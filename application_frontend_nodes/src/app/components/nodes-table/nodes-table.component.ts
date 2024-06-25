1453dbdd782d31dea27e11d49e7c736f1acdc16d
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-06-25 13:27:02 -0500

    add: paginator

--- application_frontend_nodes/src/app/components/nodes-table/nodes-table.component.ts
+++ application_frontend_nodes/src/app/components/nodes-table/nodes-table.component.ts
@@ -5,17 +5,21 @@ import { Node } from '../../data/models/node';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzIconService } from 'ng-zorro-antd/icon';
 import { EyeOutline, DeleteOutline, EditOutline } from '@ant-design/icons-angular/icons';
-
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
 @Component({
   selector: 'app-nodes-table',
   standalone: true,
-  imports: [CommonModule, NzIconModule],
+  imports: [CommonModule, NzIconModule,NzPaginationModule],
   templateUrl: './nodes-table.component.html',
   styleUrl: './nodes-table.component.css'
 })
 export class NodesTableComponent implements OnInit {
   
   nodes: Node[] = [];
+  currentNodes: Node[] = [];
+  currentPage = 1;
+  pageSize = 5;
+  totalItems = 0;
 
   constructor(private NodeService : NodeService, private iconService: NzIconService){
     this.iconService.addIcon(EyeOutline); 
@@ -28,10 +32,24 @@ export class NodesTableComponent implements OnInit {
   }
 
   loadNodes(){
-    this.NodeService.listNodes()
-                    .subscribe((nodes: Node[]) => {
+    this.NodeService.listNodes().subscribe(
+                  (nodes: Node[]) => {
                     this.nodes = nodes;
+                    this.totalItems = this.nodes.length;
+                    this.currentPage = 1;
+                    this.updateCurrentNodes();
                   });
+  }
+
+  updateCurrentNodes(): void {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.currentNodes = this.nodes.slice(startIndex, endIndex);
+  }
+
+  onPageChange(pageIndex: number): void {
+    this.currentPage = pageIndex;
+    this.updateCurrentNodes();
   }
 
 }
