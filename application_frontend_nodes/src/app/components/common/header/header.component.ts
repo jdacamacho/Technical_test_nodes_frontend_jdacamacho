363de60d2e2c01be4c1d1bc673de441b2f117242
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-06-24 21:48:50 -0500

    add: header and footer components

--- application_frontend_nodes/src/app/components/common/header/header.component.ts
+++ application_frontend_nodes/src/app/components/common/header/header.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NzGridModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
--- application_frontend_nodes/src/app/components/common/layout/layout.component.ts
+++ application_frontend_nodes/src/app/components/common/layout/layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
