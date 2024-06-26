f179fabc1d07d196b66e9aefee8136133988ed96
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-06-26 17:20:50 -0500

    add: markers

--- application_frontend_nodes/src/app/components/nodes-map/nodes-map.component.ts
+++ application_frontend_nodes/src/app/components/nodes-map/nodes-map.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { NodeService } from '../../data/services/node.service';
+import { Node } from '../../data/models/node';
 
 @Component({
   selector: 'app-nodes-map',
@@ -9,11 +11,38 @@ import { GoogleMapsModule } from '@angular/google-maps';
   templateUrl: './nodes-map.component.html',
   styleUrl: './nodes-map.component.css'
 })
-export class NodesMapComponent {
+export class NodesMapComponent implements OnInit{
+
+  nodes: Node [] =  [];
+  markers: any = [];
 
   center_Init = {
     lat: 2.43823 ,
     lng: -76.61316 
+  }
+
+  constructor(private nodeService : NodeService){}
+
+  ngOnInit(): void {
+    this.loadMarkers();
+  }
+
+  loadMarkers() : void {
+    this.loadNodes();
+
+    this.markers = this.nodes.map(node => ({
+      position: { lat: node.coordinate.valueX, lng: node.coordinate.valueY },
+      label: { color: 'blue', text: node.name },
+      title: node.name,
+    }));
+
+  }
+
+  loadNodes(): void {
+    this.nodeService.listNodes().subscribe(
+      (nodes: Node[]) => {
+        this.nodes = nodes;
+      });
   }
 
 }
