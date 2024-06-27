5f5bdc9523804361ac86b2d9cacddae32aa497a2
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-06-26 22:21:28 -0500

    add: information in map

--- application_frontend_nodes/src/app/components/nodes-map/nodes-map.component.ts
+++ application_frontend_nodes/src/app/components/nodes-map/nodes-map.component.ts
@@ -1,40 +1,46 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { GoogleMapsModule } from '@angular/google-maps';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GoogleMapsModule, MapInfoWindow, MapMarker } from '@angular/google-maps';
 import { NodeService } from '../../data/services/node.service';
 import { Node } from '../../data/models/node';
+import { ModalUpdateComponent } from '../modals/modal-update/modal-update.component';
+import { Coordinate } from '../../data/models/coordinate';
 
 @Component({
   selector: 'app-nodes-map',
   standalone: true,
-  imports: [CommonModule, GoogleMapsModule],
+  imports: [CommonModule, GoogleMapsModule,ModalUpdateComponent],
   templateUrl: './nodes-map.component.html',
   styleUrl: './nodes-map.component.css'
 })
-export class NodesMapComponent implements OnInit{
+export class NodesMapComponent implements OnInit {
 
-  nodes: Node [] =  [];
-  markers: any = [];
-  
+  @ViewChild(MapInfoWindow, { static: false }) infoWindow!: MapInfoWindow;
+
+  nodes: Node[] = [];
+  markers: any[] = [];
+  selectedMarkerName: string = '';
+  selectedMarkerInfo: string = '';
+
   center_Init = {
-    lat: 2.43823 ,
-    lng: -76.61316 
-  }
+    lat: 2.43823,
+    lng: -76.61316
+  };
 
-  constructor(private nodeService : NodeService){}
+  constructor(private nodeService: NodeService) { }
 
   ngOnInit(): void {
     this.loadNodes();
   }
 
-  loadMarkers() : void {
+  loadMarkers(): void {
     this.markers = this.nodes.map(node => ({
       position: { lat: node.coordinate.valueX, lng: node.coordinate.valueY },
       label: { color: 'black', text: node.name },
       title: node.name,
-      
+      description: node.description,
+      idNode : node.id
     }));
-
   }
 
   loadNodes(): void {
@@ -45,4 +51,9 @@ export class NodesMapComponent implements OnInit{
       });
   }
 
+  openInfoWindow(marker: any, mapMarker: MapMarker): void {
+    this.selectedMarkerName = marker.title;
+    this.selectedMarkerInfo = marker.description;
+    this.infoWindow.open(mapMarker);
+  }
 }
